ef7bc1e8b28bfd2267ca18959fd660edf2cb4b24
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-14 23:01:36 +0300

    add PciDeviceInfo.cs

---
+++ lab-1. PCI/lab-1. PCI/PciDeviceInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCI
+{
+    class PciDeviceInfo
+    {
+        private readonly string deviceId;
+
+        private readonly string vendorId;
+
+        public PciDeviceInfo(string deviceId, string vendorId)
+        {
+            this.deviceId = deviceId;
+            this.vendorId = vendorId;
+        }
+
+        public string GetDeviceId()
+        {
+            return deviceId;
+        }
+
+        public string GetVendorId()
+        {
+            return vendorId;
+        }
+    }
+}
