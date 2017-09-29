94cf196ced873aebbda270ead706a998dd3b41f2
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-09-29 16:18:54 +0300

    add BatteryViewer resources

---
+++ lab-3. Battery/Battery/Battery/View/BatteryViewer.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Battery
+{
+    partial class BatteryViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.batteryListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // batteryListBox
+            // 
+            this.batteryListBox.FormattingEnabled = true;
+            this.batteryListBox.Location = new System.Drawing.Point(7, 10);
+            this.batteryListBox.Name = "batteryListBox";
+            this.batteryListBox.Size = new System.Drawing.Size(286, 173);
+            this.batteryListBox.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(218, 189);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // BatteryViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 216);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.batteryListBox);
+            this.Name = "BatteryViewer";
+            this.Text = "BatteryViewer";
+            this.Load += new System.EventHandler(this.BatteryViewer_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox batteryListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
