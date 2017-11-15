4a5499784884a0a01f6bcf89c20213b7072e6a0d
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-11-15 22:16:28 +0300

    add DeviceProperties.cs

---
+++ lab-5. DeviceManagment/DeviceManager/DeviceManager/Api/DeviceProperties.cs
@@ -0,0 +1,14 @@
+﻿namespace DeviceManager.Api
+{
+    internal class DeviceProperties
+    {
+        public static readonly string GuIdProperty = "ClassGuid";
+        public static readonly string ManufacturerProperty = "Manufacturer";
+        public static readonly string ProviderProperty = "Caption";
+        public static readonly string DevicePathProperty = "DeviceID";
+        public static readonly string StatusProperty = "Status";
+        public static readonly string DriverDescriptionProperty = "Description";
+        public static readonly string SystemFilePathProperty = "PathName";
+        public static readonly string HardwareIdProperty = "HardwareID";
+    }
+}
